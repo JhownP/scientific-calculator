48fc0fff84ed03c9569426c32d6825565fb13e5f
Jonatas Pedroso <jonatasarlindo@hotmail.com>
2019-08-28 19:32:36 -0300

    Adicionando PowerN
    Adicionando o Calculo PowerN

--- backend/src/business/calculator/PowerNBusiness.js
+++ backend/src/business/calculator/PowerNBusiness.js
@@ -1,5 +1,7 @@
 module.exports = {
     executeCalculate(number) {
-        
+        let parse = number.split(';').map(Number);
+        let result = parse.reduce((a, b) => a ** b);
+        return result.toString();
     }
 }
